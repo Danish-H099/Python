13f3f159a201a299a1935da6d912db6ee1b80ce3
Danish <danishhussainda247@gmail.com>
2023-03-17 15:49:19 +0530

    split func

---
+++ 2 Strings/Py4_Split.py
@@ -0,0 +1,5 @@
+name = "Danish:Hussain:Dar"
+W = name.split(":")
+# Splits the string at colons(:) and form a list of splitted strings
+print(W) # op-> ['Danish', 'Hussain', 'Dar']
+print(W[1]) # op->Hussain
